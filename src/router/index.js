8c92e70e29025f4bccb0455d6313e7e4cc548642
viveloper <usik1234@gmail.com>
2020-01-16 22:54:07 +0900

    updated blog

--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,12 @@ import SignIn from "../components/SignIn.vue"
 import Section from '../components/Section.vue'
 import NotFound from '../components/NotFound.vue'
 import Post from '../components/Post.vue'
-import MarkdownViewer from '../components/MarkdownViewer.vue'
 
 const router = new VueRouter({
   mode: 'history',
   routes: [
     { path: '/', component: Home },
-    { path: '/signin', component: SignIn },
-    { path: '/mde', component: MarkdownViewer },
+    { path: '/signin', component: SignIn },    
     { path: '/sections/:section', component: Section },
     { path: '/sections/:section/posts/:id', component: Post },
     { path: '*', component: NotFound }
